a12e2988c9223d9a75a6cc28015b2cb4c9fa5ce7
rintonorma <rinto456@gmail.com>
2021-12-30 14:37:04 +0700

    homepage heading text

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
   });
 
   $('.game-slider').slick({
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
@@ -51,7 +51,7 @@ $(document).ready(function () {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
         }
       },
