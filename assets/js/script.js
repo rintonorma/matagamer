46f9d2844f2d06d738bcbde054143f3250206c92
rintonorma <rinto456@gmail.com>
2022-01-04 11:42:08 +0700

    single product related

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,6 +94,40 @@ $(document).ready(function () {
     nextArrow: '<button class="slide-arrow next-arrow"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
   });
 
+  $('.product-related').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"><ion-icon name="chevron-back-outline"></ion-icon></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+
   $(document).scroll(function () {
     var $nav = $(".navbar.fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
